f62974882031f8de2d992d7857de5656b12fcd46
Alexandr Cherednichenko <alexandr2110pro@gmail.com>
2018-02-01 05:24:15 +0300

    change

---
+++ src/app/modules/users/users.service.spec.ts
@@ -0,0 +1,12 @@
+import { UsersService } from './users.service';
+
+jest.setTimeout(15000);
+
+describe('UsersService', () => {
+
+  it('should run', () => {
+
+    expect(true).toBe(true);
+  });
+
+});
